d3bddd7cb92c73a8debd4f1cd53251d5c548426e
Frederic Lossignol <frederic.lossignol@gmail.com>
2023-01-31 16:41:09 +0100

    créer une méthode d'acces aux donnees dans un service

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,3 +1,5 @@
+
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,10 +7,12 @@ import { Injectable } from '@angular/core';
 })
 export class MovieService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+  //  l'objet http de la class HttpClient
+  // dispose des méthodes get(), post(), delete(), put(), ....
 
   getMoviesFromApi() {
-    
+    return this.http.get('https://api.themoviedb.org/3/discover/movie?api_key=efdeb661aaa006b1e4f36f990a5fd8fd&language=fr');
   }
 
 
