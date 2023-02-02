57da1bc678274b9010c9f58bf2d77ee6740b342d
Frederic Lossignol <frederic.lossignol@gmail.com>
2023-02-02 14:25:15 +0100

    Dans le component DetailComponent Récuperation de l'ID de l'url et du film dans movies$ dans movie.service

--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { MovieService } from '../services/movie.service';
+import { MovieModel } from '../shared/models/movie.model';
 
 @Component({
   selector: 'app-detail',
@@ -8,12 +10,27 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class DetailComponent {
 
-  constructor(private route:ActivatedRoute) {}
+  movie:any = {};
+  movieId:number = 0;
+
+  constructor(
+    private route:ActivatedRoute, 
+    private movieSvc:MovieService) {}
+
 
   ngOnInit() {
 
+
+
     console.log( this.route.snapshot.params) // {id: 123456}
-    
+    this.movieId = this.route.snapshot.params['id'];
+
+    this.movieSvc.getMovies$().subscribe( (movies:MovieModel[]) => {
+      this.movie = movies.find( (movie) => movie.id == this.movieId);
+      console.log(this.movie);
+    })
+
+
 
   }
 
